a07071d323bd35e3f222ffcaec4c4e3434e6035d
cristiancibotariu <cristian.cibotariu2@gmail.com>
2023-11-24 13:37:59 +0200

    update final partial

--- partial/sub1.py
+++ partial/sub1.py
@@ -8,38 +8,38 @@ import random
 # from scipy import stats
 # import arviz as az
 
-runde = 0
-vP0 = 0
-vP1 = 0
-token = 0
+runde = 0 
+vP0 = 0 #numarul de victorii ale primului jucator
+vP1 = 0 #numarul de victorii ale celui de-al doilea jucator
+token = 0 #token ul care arata cine incepe
 
 if random.random() >= 0.5:
-    token = 1
+    token = 1 #decizia legata de cine incepe
 
 while runde < 20000:
     m = 0
     n = 0
-    if token == 1:
+    if token == 1: #incepe jucatorul P1
         if random.random() <= 0.5:
             n = 1
-        if n == 1:
-            for i in range(2):
+        if n == 1: 
+            for i in range(2):#jucatorul P0 da de 2 ori
                 if random.random() <= 0.33:
                     m += 1
         else:
             if random.random() <= 0.33:
                     m += 1
-    else:
+    else: #incepe P0
         if random.random() <= 0.33:
             n = 1
         if n == 1:
-            for i in range(2):
+            for i in range(2): #P1 da de 2 ori
                 if random.random() <= 0.5:
                     m += 1
-        else: 
+        else:  # P1 da o singura data
             if random.random() <= 0.5:
                     m += 1
-    if n>m:
+    if n>m: # calculul numarului de victorii
         if token == 0:
             vP0 += 1
         else: vP1 += 1
@@ -53,10 +53,10 @@ if vP0 > vP1:
     print("Jucatorul P0 are mai multe sanse de victorie")
 else: print("Jucatorul P1 are mai multe sanse de victorie")
 
-model = BayesianNetwork([('coin_toss', 'P1'),('coint_toss','P0')])
+model = BayesianNetwork([('coin_toss', 'P1'),('coint_toss','P0')]) #realizarea retelei
 
 coin_toss = TabularCPD(variable='coin_toss', variable_card=2,
-                    values=[[0.5],[0.5]])
+                    values=[[0.5],[0.5]]) #event cine incepe
 
 P0 = TabularCPD(variable='P0', variable_card=2,
                     values=[[0.33, 0.11, 0.22, 0.44], [0.66, 0.99, 0.88, 0.66]], #ori dai stema/ban, ori ss, ori sb, ori bb ori bs
@@ -72,11 +72,8 @@ model.add_cpds(coin_toss, P0, P1)
 assert model.check_model()
 
 infer = VariableElimination(model)
-prob_P0 = infer.query(variables=['P0'], evidence={'P1' : 1})
+prob_P0 = infer.query(variables=['P0'], evidence={'P1' : 1}) #incepe jucatorul p0 iar p1 are urmatoarele probabilitati
 print(prob_P0)
-
-# prob_cutremur_stiind_alarma = infer.query(variables=['cutremur'], evidence={'alarma': 1})
-# print(prob_cutremur_stiind_alarma)
 
 pos = nx.circular_layout(model)
 nx.draw(model, pos=pos, with_labels=True, node_size=4000, font_weight='bold', node_color='skyblue')
--- partial/sub2.py
+++ partial/sub2.py
@@ -5,15 +5,17 @@ import arviz as az
 from scipy import stats
 
 
-def main():
-    miu = 5
-    sigma = 2
+if __name__ == '__main__':
+    #miu = 5 valori random pt punctul 1
+    #sigma = 2
 
     model = pm.Model()
     with model:
+        miu = pm.Poisson("miu", mu=10) #urm 2 linii folosim distributii pentru a genera miu si sigma
+        sigma = pm.Poisson("sigma", mu=10)
         avg_wait = pm.Normal("average wait time", miu, sigma)
 
-        trace = pm.sample(200)
+        trace = pm.sample(200) #generam 200 de timpi
 
-az.plot_posterior(trace)
-plt.show()
+    az.plot_posterior(trace)
+    plt.show()
